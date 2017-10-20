8a0faa7f5689777d8b57719e01d757dd3c82961a
Mortif3767 <mortif3767@outlook.com>
2017-10-20 11:38:41 +0800

    login

---
+++ account/forms.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    #用passwordinput控件来渲染html input元素,属性为type="password"
--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            #authenticate方法返回一个用户对象
+            if user is not None:
+                if user.is_active:
+                    login(request, user) #login方法将用户设置到session中
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+        else:
+            return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
