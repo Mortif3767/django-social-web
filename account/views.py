5a128955e7c43cd2e83d4390fa78247c419c46c6
Mortif3767 <mortif3767@outlook.com>
2017-10-20 14:16:46 +0800

    fixed

--- account/views.py
+++ account/views.py
@@ -19,8 +19,8 @@ def user_login(request):
                     return HttpResponse('Authenticated successfully')
                 else:
                     return HttpResponse('Disabled account')
-        else:
-            return HttpResponse('Invalid login')
+            else:
+                return HttpResponse('Invalid login')
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
